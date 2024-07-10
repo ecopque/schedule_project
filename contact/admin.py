b1f15485cbbf6cefa37f5649df1f9d9d77fd0976
Edson Copque <ecop@mailfence.com>
2024-07-10 18:11:29 -0300

    Updated important: contact/admin.py and models.py.

--- contact/admin.py
+++ contact/admin.py
@@ -5,6 +5,6 @@
 from django.contrib import admin
 from contact.models import cls_contact # my_project/contact/models.py
 
-@admin.register()
+@admin.register(cls_contact)
 class cls_contactadmin(admin.ModelAdmin):
     ...
--- contact/models.py
+++ contact/models.py
@@ -6,9 +6,13 @@ from django.utils import timezone
 # id (primary key - automatic);
 # first_name (string), last_name (string), phone (string), e-mail (email), created_date (date), description (text), category (foreing key), show (boolean), owner (foreing key) and picture (image).
 class cls_contact(models.Model):
-    var_firstname = models.CharField(max_length=50)
-    var_lastname = models.CharField(max_length=50, blank=True)
-    var_phone = models.CharField(max_length=20)
-    var_email = models.EmailField(max_length=254)
-    var_createddate = models.DateTimeField(default=timezone.now)
-    var_description = models.TextField(blank=True)
+    var_firstname = models.CharField(name='First Name', max_length=50)
+    var_lastname = models.CharField(name='Last Name', max_length=50, blank=True)
+    var_phone = models.CharField(name='Phone Number', max_length=20)
+    var_email = models.EmailField(name='E-mail', max_length=254)
+    var_createddate = models.DateTimeField(name='Create Date', default=timezone.now)
+    var_description = models.TextField(name='Description', blank=True)
+
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
