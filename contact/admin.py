61804372e0a2f0e977383adfc3f9ab2371c4937c
Edson Copque <ecop@mailfence.com>
2024-07-10 17:50:44 -0300

    Updated: contact/admin.py.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
-
 # Register your models here.
+
+#my_project/contact/admin.py
+
+from django.contrib import admin
+from contact.models import cls_contact # my_project/contact/models.py
+
+@admin.register()
+class cls_contactadmin(admin.ModelAdmin):
+    ...
