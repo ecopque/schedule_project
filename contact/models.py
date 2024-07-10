5166c63bc1d89e7c5e39905ad485bed2931d6007
Edson Copque <ecop@mailfence.com>
2024-07-10 19:34:03 -0300

    Change contac/models.py: Removed verbose_name from cls_contact() class variables.

--- contact/models.py
+++ contact/models.py
@@ -6,13 +6,16 @@ from django.utils import timezone
 # id (primary key - automatic);
 # first_name (string), last_name (string), phone (string), e-mail (email), created_date (date), description (text), category (foreing key), show (boolean), owner (foreing key) and picture (image).
 class cls_contact(models.Model):
-    var_firstname = models.CharField(name='First Name', max_length=50)
-    var_lastname = models.CharField(name='Last Name', max_length=50, blank=True)
-    var_phone = models.CharField(name='Phone Number', max_length=20)
-    var_email = models.EmailField(name='E-mail', max_length=254)
-    var_createddate = models.DateTimeField(name='Create Date', default=timezone.now)
-    var_description = models.TextField(name='Description', blank=True)
+    var_firstname = models.CharField(max_length=50)
+    var_lastname = models.CharField(max_length=50, blank=True)
+    var_phone = models.CharField(max_length=20)
+    var_email = models.EmailField(max_length=254)
+    var_createddate = models.DateTimeField(default=timezone.now)
+    var_description = models.TextField(blank=True)
 
     class Meta: #cls_contact()
         verbose_name = 'Contact'
         verbose_name_plural = 'Contacts'
+    
+    def __str__(self) -> str:
+        return f'{self.var_firstname} {self.var_lastname}'
