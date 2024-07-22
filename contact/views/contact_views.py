e4fb90275a17bbf4b50f26dcd1ae00f0eb994516
Edson Copque <ecop@mailfence.com>
2024-07-22 16:17:17 -0300

    Updated: contact_views and deleted_views.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,2 +1,6 @@
 # my_project/contact/views/contact_views.py
 
+from django.shortcuts import render
+
+def func_index(request):
+    return render(request, 'contact/index.html',) # my_project/contact/templates/contact/index.html
