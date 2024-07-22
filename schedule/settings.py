1187546d1dd8ce29f7e379a769bc22687eb7a5df
Edson Copque <ecop@mailfence.com>
2024-07-22 15:46:20 -0300

    Updated: .gitignore and schedule/settings.py(import local_settings.py).

--- schedule/settings.py
+++ schedule/settings.py
@@ -132,4 +132,8 @@ MEDIA_ROOT = BASE_DIR / 'media' # my_project/media
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Need commit.
+
+try:
+    from schedule.local_settings import *
+except ImportError:
+    ...
