e7cc92851de59aa9d6f439a5ee9a939cde18aefc
Edson Copque <ecop@mailfence.com>
2024-07-25 20:16:06 -0300

    Updated contact.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from contact.models import cls_contact # my_project/contact/models.py
+from django.http import Http404
 
 def func_index(request):
     # contacts = cls_contact.objects.all().order_by('-id')
@@ -12,7 +13,9 @@ def func_index(request):
     return render(request, 'contact/index.html', context) # my_project/contact/templates/contact/index.html
 
 def func_contact(request, contact_id): ##
-    single_contact = cls_contact.objects.get(pk=contact_id) ##
+    single_contact = cls_contact.objects.filter(pk=contact_id, show=True).first() ##
+    if single_contact is None: ##
+        raise Http404() ##
     context = {'contact': single_contact,} ## # my_project/contact/templates/contact/contact.html
 
     return render(request, 'contact/contact.html', context) ##
