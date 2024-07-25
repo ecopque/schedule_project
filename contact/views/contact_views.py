eeffaf83c22c5ee0c6fbb381a4aa58fcf7c6ee14
Edson Copque <ecop@mailfence.com>
2024-07-25 10:27:25 -0300

    Sorting views in descending order.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from contact.models import cls_contact # my_project/contact/models.py
 
 def func_index(request):
-    contacts = cls_contact.objects.all() #AQUI!
+    contacts = cls_contact.objects.all().order_by('-id')
     context = {'contacts': contacts,} #AQUI!
 
     return render(request, 'contact/index.html', context) #AQUI! # my_project/contact/templates/contact/index.html
