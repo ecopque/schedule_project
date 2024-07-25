062265ca13c0269ee1c5ddb00c92d0b8a55c5ffe
Edson Copque <ecop@mailfence.com>
2024-07-25 14:24:21 -0300

    Single-contact construction.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -5,8 +5,14 @@ from contact.models import cls_contact # my_project/contact/models.py
 
 def func_index(request):
     # contacts = cls_contact.objects.all().order_by('-id')
-    contacts = cls_contact.objects.filter(show=True).order_by('-id') ##
-    print(contacts.query) ##
+    contacts = cls_contact.objects.filter(show=True).order_by('-id')
+    print(contacts.query)
     context = {'contacts': contacts,}
 
     return render(request, 'contact/index.html', context) # my_project/contact/templates/contact/index.html
+
+def func_contact(request): ##
+    single_contact = cls_contact.objects.get() ##
+    context = {'contact': single_contact,} ## # my_project/contact/templates/contact/contact.html
+
+    return render(request, 'contact/contact.html', context) ##
