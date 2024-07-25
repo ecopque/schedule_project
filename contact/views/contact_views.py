c04c74930c6b0bebac93c6277f296022c4571b3e
Edson Copque <ecop@mailfence.com>
2024-07-25 14:06:30 -0300

    Code cleaning.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,6 +6,7 @@ from contact.models import cls_contact # my_project/contact/models.py
 def func_index(request):
     # contacts = cls_contact.objects.all().order_by('-id')
     contacts = cls_contact.objects.filter(show=True).order_by('-id') ##
+    print(contacts.query) ##
     context = {'contacts': contacts,}
 
     return render(request, 'contact/index.html', context) # my_project/contact/templates/contact/index.html
