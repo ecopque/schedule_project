f1c8fad71025d14c52acf5134d93f7145ddceec1
Edson Copque <ecop@mailfence.com>
2024-07-25 15:06:00 -0300

    Building contact.

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,10 @@
 # my_project/contact/urls.py
 
 from django.urls import path
-from contact import views as contact_views # my_project/contact/views.py
+from contact import views as contact_views # my_project/contact/contact_views.py
 
 # app_name = 'contact'
 urlpatterns = [
-     path('', contact_views.func_index, name='contact_index'), # my_project/contact/views.py
-     path('', contact_views.func_contact, name='contact') ##
+     path('', contact_views.func_index, name='contact_index'), # my_project/contact/contact_views.py
+     path('<int:contact_id>/', contact_views.func_contact, name='contact_contact') ##  ##AAA # my_project/contact/contact_views.py
 ]
