25a77ba0083d9ebb45d95ef19a0059c2111cf971
Edson Copque <ecop@mailfence.com>
2024-07-25 10:31:43 -0300

    Enabling show in admin/contact.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from contact.models import cls_contact # my_project/contact/models.py
 
 def func_index(request):
-    contacts = cls_contact.objects.all().order_by('-id')
-    context = {'contacts': contacts,} #AQUI!
+    # contacts = cls_contact.objects.all().order_by('-id')
+    contacts = cls_contact.objects.filter(show=True).order_by('-id') ##
+    context = {'contacts': contacts,}
 
-    return render(request, 'contact/index.html', context) #AQUI! # my_project/contact/templates/contact/index.html
+    return render(request, 'contact/index.html', context) # my_project/contact/templates/contact/index.html
