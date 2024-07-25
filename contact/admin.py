e63dc7778af18643cbf71da63f1698adce5abd66
Edson Copque <ecop@mailfence.com>
2024-07-25 10:50:47 -0300

    Enabling admin/contact show for multiple contacts.

--- contact/admin.py
+++ contact/admin.py
@@ -12,11 +12,11 @@ class cls_categoryadmin(admin.ModelAdmin):
 
 @admin.register(cls_contact)
 class cls_contactadmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone',) # my_project/contact/models.py
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'show',) ## # my_project/contact/models.py
     ordering = ('-id',)
     list_filter = ('created_date',) # my_project/contact/models.py
     search_fields = ('id', 'first_name', 'last_name',) # my_project/contact/models.py
     list_per_page = 50
     list_max_show_all = 1000
-    # list_editable = ('first_name', 'last_name',) # my_project/contact/models.py
+    list_editable = ('first_name', 'last_name', 'show') ## # my_project/contact/models.py
     list_display_links = ('id', 'phone',) # my_project/contact/models.py
