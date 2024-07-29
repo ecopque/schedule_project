51314e2c298e8179bea49cbd7185b90eba5d6b74
Edson Copque <ecop@mailfence.com>
2024-07-29 15:30:03 -0300

    Update.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -27,7 +27,7 @@ def func_search(request): ##
     search_value = request.GET.get('q', '').strip() ## # my_project/base_templates/partials/_header.html
     print('search_value: ', search_value)
     if search_value == '': ##
-        return redirect('contact_index') ##
+        return redirect('contact_index') ## # my_project/contact/urls.py
     contacts = cls_contact.objects.filter(show=True).filter(Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) |  Q(phone__icontains=search_value) |  Q(email__icontains=search_value)).order_by('-id') ## # my_project/contact/models.py
     context = {'contacts': contacts, 'site_title': 'Search - '} ##
     return render(request, 'contact/index.html', context) ##
