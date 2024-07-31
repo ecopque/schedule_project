e9ba682066eae9beab5979c7c1480544eede934f
Edson Copque <ecop@mailfence.com>
2024-07-31 15:38:00 -0300

    Backup.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,12 +6,18 @@ from django.db.models import Q
 from contact.models import cls_contact # my_project/contact/models.py
 from django.http import Http404
 from django.core.paginator import Paginator
+from django import forms ##
+
+class cls_contactform(forms.ModelForm): ##
+    class Meta: ##
+        model = cls_contact ##
+        fields = ('first_name', 'last_name', 'phone',) ##
 
 def func_create(request):
-    if request.method == 'POST':
-        print(request.method)
-        print(request.POST.get('first_name')) # my_project/contact/templates/contact/create.html
-        print(request.POST.get('last_name')) # my_project/contact/templates/contact/create.html
+    # if request.method == 'POST':
+    #     print(request.method)
+    #     print(request.POST.get('first_name')) # my_project/contact/templates/contact/create.html
+    #     print(request.POST.get('last_name')) # my_project/contact/templates/contact/create.html
 
 
     context = {}
