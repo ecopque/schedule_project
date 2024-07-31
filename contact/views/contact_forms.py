d0c22394423266457fcdbde79a529ee0a724f41d
Edson Copque <ecop@mailfence.com>
2024-07-31 09:41:17 -0300

    Backup.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,2 +1,12 @@
 # my_project/contact/views/contact_forms.py
 
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.db.models import Q
+from contact.models import cls_contact # my_project/contact/models.py
+from django.http import Http404
+from django.core.paginator import Paginator
+
+def func_create(request):
+    context = {}
+    return render(request, 'contact/create.html', context)
