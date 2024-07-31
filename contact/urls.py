d85c25c83fbe83e7ec0ec8555a9f62b73933de4e
Edson Copque <ecop@mailfence.com>
2024-07-31 09:31:48 -0300

    Fixed contact_views import.

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,8 @@
 # my_project/contact/urls.py
 
 from django.urls import path
-from contact import views as contact_views # my_project/contact/contact_views.py
+# from contact import views as contact_views # my_project/contact/contact_views.py
+from contact.views import contact_views
 
 # app_name = 'contact'
 urlpatterns = [
@@ -9,8 +10,8 @@ urlpatterns = [
      path('search/', contact_views.func_search, name='contact_search'), # my_project/contact/contact_views.py
 
      # Contact CRUD
-     path('contacts/create', contact_views.func_contact, name='contact_create'),
+     # path('contacts/create', contact_views.func_contact, name='contact_create'),
      path('contacts/<int:contact_id>/read', contact_views.func_contact, name='contact_contact'), # my_project/contact/contact_views.py
-     path('contacts/<int:contact_id/update', contact_views.func_contact, name='contact_update'),
-     path('contacts/<int:contact_id/delete', contact_views.func_contact, name='contact_delete'),
+     # path('contacts/<int:contact_id/update', contact_views.func_contact, name='contact_update'),
+     # path('contacts/<int:contact_id/delete', contact_views.func_contact, name='contact_delete'),
 ]
