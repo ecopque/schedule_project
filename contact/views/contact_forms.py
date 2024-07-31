7d0562fcd7b07a9a6e55d3f37b37ae938b7e25b8
Edson Copque <ecop@mailfence.com>
2024-07-31 14:48:55 -0300

    Backup.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -8,5 +8,11 @@ from django.http import Http404
 from django.core.paginator import Paginator
 
 def func_create(request):
+    if request.method == 'POST':
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+
+
     context = {}
     return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
