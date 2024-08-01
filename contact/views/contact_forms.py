971941c7d63adb5f24b0647218fe0ebfb5676bfd
Edson Copque <ecop@mailfence.com>
2024-08-01 10:28:30 -0300

    Updated POST and GET methods.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -19,5 +19,11 @@ def func_create(request):
     #     print(request.POST.get('first_name')) # my_project/contact/templates/contact/create.html
     #     print(request.POST.get('last_name')) # my_project/contact/templates/contact/create.html
 
+    # POST method
+    if request.method == 'POST':
+        context = {'form': cls_contactform(data=request.POST)} ##
+        return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
+    
+    # GET method
     context = {'form': cls_contactform()} ##
     return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
