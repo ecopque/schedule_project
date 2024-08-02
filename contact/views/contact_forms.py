bda025de10cd0976dccf10c6cd84d793d2c45091
Edson Copque <ecop@mailfence.com>
2024-08-02 15:36:28 -0300

    Code cleanup.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,17 +3,6 @@
 from django.shortcuts import render
 from contact.forms import cls_contactform # my_project/contact/forms.py
 
-# Moved to forms.py
-# class cls_contactform(forms.ModelForm):
-#     class Meta:
-#         model = cls_contact
-#         fields = ('first_name', 'last_name', 'phone',) # (# my_project/contact/templates/contact/create.html)
-
-#     def clean(self):
-#         cleaned_data = self.cleaned_data
-#         self.add_error('first_name', ValidationError('Error message.', code='invalid'))
-#         return super().clean()
-
 def func_create(request):
     # if request.method == 'POST':
     #     print(request.method)
