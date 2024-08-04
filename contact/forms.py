c339ab97fd88596407196edfc48491643c7af563
Edson Copque <ecop@mailfence.com>
2024-08-04 19:11:37 -0300

    Code cleanup.

--- contact/forms.py
+++ contact/forms.py
@@ -9,7 +9,7 @@ class cls_contactform(forms.ModelForm):
         model = cls_contact
         fields = ('first_name', 'last_name', 'phone',) # (# my_project/contact/templates/contact/create.html)
 
-        widgets = {'first_name': forms.PasswordInput()} ##
+        widgets = {'first_name': forms.PasswordInput()} ###
 
     def clean(self):
         # cleaned_data = self.cleaned_data
