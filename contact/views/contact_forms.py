d650ccc538ddd61d527d222c00d3534c4d27cc1b
Edson Copque <ecop@mailfence.com>
2024-08-07 17:21:09 -0300

    Under construction.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from contact.forms import cls_contactform # my_project/contact/forms.py
 from django.shortcuts import redirect
 from django.urls import reverse ##
-from django.shortcuts import get_list_or_404 ##
+from django.shortcuts import get_object_or_404 ##
 from contact.forms import cls_contact ##
 
 def func_create(request):
@@ -33,20 +33,20 @@ def func_create(request):
     return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
 
 def func_update(request, contact_id):
-    contact = get_list_or_404(cls_contact, pk=contact_id, show=True) ## # my_project/contact/models.py
+    contact = get_object_or_404(cls_contact, pk=contact_id, show=True) ## # my_project/contact/models.py
     form_action = reverse('contact_update', args=(contact_id,)) ##
     
     if request.method == 'POST':
-        form = cls_contactform(data=request.POST, instance=contact) ##
+        form = cls_contactform(request.POST, instance=contact) ##
         context = {'form':form, 'form_action': form_action,} #% my_project/contact/templates/contact/create.html
 
         if form.is_valid():
             contact = form.save(commit=False)
             contact.show = False
             contact.save()
-            return redirect('contact_update', contact_id=contact.pk) # my_project/contact/urls.py
+            return redirect('contact_update', contact_id=contact.pk) ## # my_project/contact/urls.py
 
         return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
 
-    context = {'form':cls_contactform(isinstance=contact), 'form_action': form_action,} ##
+    context = {'form':cls_contactform(instance=contact), 'form_action': form_action,} ##
     return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
