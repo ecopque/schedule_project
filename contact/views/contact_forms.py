d7f9b48d1107ec54f54d94378a8058d76549988e
Edson Copque <ecop@mailfence.com>
2024-08-07 09:24:49 -0300

    Saving forms.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from contact.forms import cls_contactform # my_project/contact/forms.py
+from django.shortcuts import redirect ##
 
 def func_create(request):
     # if request.method == 'POST':
@@ -15,8 +16,13 @@ def func_create(request):
         context = {'form':form} ## # my_project/contact/forms.py
 
         if form.is_valid(): ##
-            print('Form is valid.')
-            form.save() ##
+            # form.save()
+
+            contact = form.save(commit=False) ##
+            contact.show = False ##
+            contact.save() ##
+            return redirect('contact_create') # my_project/contact/urls.py
+
 
         return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
     
