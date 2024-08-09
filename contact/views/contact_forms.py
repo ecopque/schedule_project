17a99ccc6408e832b3b5a45a318dd18fa5f5d4fd
Edson Copque <ecop@mailfence.com>
2024-08-09 15:26:43 -0300

    Under construction.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -12,22 +12,18 @@ def func_create(request):
     #     print(request.method)
     #     print(request.POST.get('first_name')) # my_project/contact/templates/contact/create.html
     #     print(request.POST.get('last_name')) # my_project/contact/templates/contact/create.html
-
     # POST method
     form_action = reverse('contact_create') # my_project/contact/urls.py
     if request.method == 'POST':
         form = cls_contactform(data=request.POST)
         context = {'form':form, 'form_action': form_action,} #% my_project/contact/templates/contact/create.html
-
         if form.is_valid():
             contact = form.save(commit=False)
             contact.show = True
             contact.save()
             # contact = form.save()
             return redirect('contact_update', contact_id=contact.pk) # my_project/contact/urls.py
-
         return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
-
     # GET method
     # context = {'form': cls_contactform()}
     context = {'form':cls_contactform(), 'form_action': form_action,}
@@ -36,18 +32,23 @@ def func_create(request):
 def func_update(request, contact_id):
     contact = get_object_or_404(cls_contact, pk=contact_id, show=True) # my_project/contact/models.py
     form_action = reverse('contact_update', args=(contact_id,))
-    
     if request.method == 'POST':
         form = cls_contactform(request.POST, instance=contact)
         context = {'form':form, 'form_action': form_action,} #% my_project/contact/templates/contact/create.html
-
         if form.is_valid():
             contact = form.save(commit=False)
             contact.show = False
             contact.save()
             return redirect('contact_update', contact_id=contact.pk) # my_project/contact/urls.py
-
         return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
-
     context = {'form':cls_contactform(instance=contact), 'form_action': form_action,}
     return render(request, 'contact/create.html', context) # my_project/contact/templates/contact/create.html
+
+def func_delete(request, contact_id): ##
+    contact = get_object_or_404(cls_contact, pk=contact_id, show=True) ##
+    confirmation = request.POST.get('Confirmation', 'no') ##
+    print('Confirmation', confirmation)
+    if confirmation == 'yes':
+        contact.delete() ##
+        return redirect('contact_index') ##
+    return render(request, 'contact/contact.html', {'contact': contact}) ##
