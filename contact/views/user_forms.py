26858f23da68efb6a211a0ab7f85c7fb0212a241
Edson Copque <ecop@mailfence.com>
2024-08-11 20:58:38 -0300

    Code cleanup.

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,12 +1,12 @@
 # my_project/contact/views/user_forms.py
 
 from django.shortcuts import render
-from contact.forms import cls_registerform ## # my_project/contact/forms.py
+from contact.forms import cls_registerform # my_project/contact/forms.py
 
-def func_register(request): ##
-    form = cls_registerform() ##
-    if request.method == 'POST': ##
-        form = cls_registerform(request.POST) ##
-        if form.is_valid(): ##
-            form.save() ##
-    return render(request, 'contact/register.html', {'form': form}) ## # my_project/contact/templates/contact/register.html
+def func_register(request):
+    form = cls_registerform()
+    if request.method == 'POST':
+        form = cls_registerform(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'contact/register.html', {'form': form}) # my_project/contact/templates/contact/register.html
