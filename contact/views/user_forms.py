c1f23d03e4bf85ae8c2fe0cab06e6c007c607bc9
Edson Copque <ecop@mailfence.com>
2024-08-11 18:57:14 -0300

    Under construction.

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -5,4 +5,8 @@ from contact.forms import cls_registerform ## # my_project/contact/forms.py
 
 def func_register(request): ##
     form = cls_registerform() ##
+    if request.method == 'POST': ##
+        form = cls_registerform(request.POST) ##
+        if form.is_valid(): ##
+            form.save() ##
     return render(request, 'contact/register.html', {'form': form}) ## # my_project/contact/templates/contact/register.html
