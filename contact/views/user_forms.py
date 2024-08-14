4f1dd98f3475c88432efa672933d7f69df57a837
Edson Copque <ecop@mailfence.com>
2024-08-14 10:01:15 -0300

    Under construction.

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from contact.forms import cls_registerform # my_project/contact/forms.py
 from django.contrib import messages ##
+from django.shortcuts import redirect ##
 
 def func_register(request): #% my_project/contact/urls.py
     form = cls_registerform()
@@ -16,4 +17,6 @@ def func_register(request): #% my_project/contact/urls.py
         form = cls_registerform(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Registered user.') ##
+            return redirect('contact_index') ##
     return render(request, 'contact/register.html', {'form': form}) # my_project/contact/templates/contact/register.html
