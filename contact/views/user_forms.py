be3735b8c2cd70e0fcaf4e681d3288681a5d7848
Edson Copque <ecop@mailfence.com>
2024-08-14 09:46:50 -0300

    Under construction.

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -2,9 +2,13 @@
 
 from django.shortcuts import render
 from contact.forms import cls_registerform # my_project/contact/forms.py
+from django.contrib import messages ##
 
 def func_register(request): #% my_project/contact/urls.py
     form = cls_registerform()
+    
+    messages.info(request, 'Any text.') ##
+
     if request.method == 'POST':
         form = cls_registerform(request.POST)
         if form.is_valid():
