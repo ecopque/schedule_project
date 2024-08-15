eaa9f7d91b47e8844314e4330119c8e08d90a118
Edson Copque <ecop@mailfence.com>
2024-08-15 20:28:01 -0300

    Clean password1: under construction.

--- contact/forms.py
+++ contact/forms.py
@@ -80,10 +80,10 @@ class cls_registerupdateform(forms.ModelForm):
         return email
 
     def clean_password1(self):
-        password1 = self.cleaned_data.get('password1')
+        password1 = self.cleaned_data.get('password1') ##
         if password1:
             try:
-                password_validation.validate_password(password1)
-            except ValidationError as errors:
-                self.add_error('password', ValidationError(errors))
+                password_validation.validate_password(password1) ##
+            except ValidationError as errors: ##
+                self.add_error('password1', ValidationError(errors)) ##
         return password1
